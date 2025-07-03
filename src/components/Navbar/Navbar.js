20d883226a1ea3a46b7577f8151b3b6439d71882
Neville Pethani <nevillepeth@gmail.com>
2025-07-02 23:45:27 -0400

    Cool navbar achieved

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Navbar.css';
 
 function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      
+      if (window.scrollY > 1) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <div className="Navbar">
+    <div className={`Navbar ${scrolled ? 'Navbar-scrolled' : ''}`}>
       <ul>
         <li><a href="#about-me">About Me</a></li>
         <li><a href="#projects">Projects</a></li>
